3af4328becf30ea138ac5098e2850d26c1391536
kartram2 <karthik.ramachand@outlook.com>
2017-12-28 14:29:46 -0800

    Added search icon in search bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Create a db instance
+db = SQLAlchemy(app)
+
+# Create a db migration instance
+db_migrate = Migrate(app, db)
+
+from app import routes, models
--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config (object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+
+    # Set the path to the database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # No need to signal the app everytime a change is about to be made to the
+    # database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
